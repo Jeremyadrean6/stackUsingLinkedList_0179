ad3c87aa29a02bf2fffb1b94e93bbf15512096ee
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-22 10:34:51 +0700

    menambahkan push operation

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -14,14 +14,23 @@ class Node {
 };
 
 // Stack class
-class stack {
+class Stack {
     private:
     Node* top; // Pointer to the top node pf the stack 
 
     public:
-    stack() {
+    Stack() {
         top = NULL; // Initialize the stuck with a null top pointer
     }
     
-   
+    // push operation: insert an elemnt onto the top of the stack
+    int push(int value) {
+        Node* newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. Assign value 
+        newNode->next = top; // 3. set the next pointer of the new node to the current top
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "push value: "<< value << endl;
+        return value;
+    }
+
 };
