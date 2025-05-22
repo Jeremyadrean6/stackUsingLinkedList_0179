9944754b0b558574b5b2e2732d7ce7f81a953b5d
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-22 10:58:40 +0700

    menambahkan int main

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -69,3 +69,12 @@ class Stack {
         return top == NULL; // Return true if the top pointer si NULL, indicating an empty stack
     }
 };
+
+int main()
+{
+    Stack stack;
+
+    int choice = 0;
+    int value;
+    
+}
