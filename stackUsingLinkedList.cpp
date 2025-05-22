a76454e73d9376fd31b8f0337bcdded3669d1d29
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-22 10:45:25 +0700

    menambahkan Node pada pop operation

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -39,5 +39,8 @@ class Stack {
             cout << "stack is empty." << endl;
         }
 
+        Node* temp = top; // Create a temporary pointer to the top node 
+        top = top->next; // Update the top pointer to the next node
+        cout << "popped value: "<< top->data << endl;
     }
 };
