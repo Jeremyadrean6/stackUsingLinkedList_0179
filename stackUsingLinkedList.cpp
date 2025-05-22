79f0767f784c94a02fb53eb4f903919235ac0c25
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-22 11:03:55 +0700

    menambahkan stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -91,6 +91,9 @@ int main()
         case 1:
             cout << "Enter the value to push: ";
             cin >> value;
+            stack.push(value); // Push the entered value onto the stack
+            break;
         }
     }
+    
 }
